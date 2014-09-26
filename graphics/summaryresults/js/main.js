92d920be4a449efd0bdc8bdc6f2c74152c16a510
Gabriel Florit <gabrielflorit@gmail.com>
2014-09-26 13:40:42 -0400

    initial summaryresults commit

---
+++ graphics/summaryresults/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".homepage-graphic.summaryresults").length', function() {
+
+	var masterSelector = '.homepage-graphic.summaryresults';
+	var master = $(masterSelector);
+
+}); }());
