baa9fd1663464e4082d786ac8b858c9a4a32319a
Gabriel Florit <gabrielflorit@gmail.com>
2014-09-29 15:01:31 -0400

    fetch data based on race-numbers

--- common/js/jsx/RacesSummaryResultsPolling.jsx
+++ common/js/jsx/RacesSummaryResultsPolling.jsx
@@ -5,13 +5,31 @@
 var React = require('react');
 
 var RacesSummaryResultsPolling = React.createClass({
+	fetchData: function() {
+		var url = 'http://www.bostonglobe.com/electionapi/races/number?' + this.props.raceNumbers.map(function(value) {
+			return 'number=' + value;
+		}).join('&');
 
+		$.ajax({
+			url: url,
+			dataType: 'jsonp',
+			jsonpCallback: '_' + this.props.raceNumbers.join('_')
+		});
+	},
+	getInitialState: function() {
+		return {races: []};
+	},
+	componentDidMount: function() {
+		window['_' + this.props.raceNumbers.join('_')] = function(json) {
+			this.setState({races:json});
+		}.bind(this);
+		this.fetchData();
+	},
 	render: function() {
 		return (
 			<div>{this.props.raceNumbers}</div>
 		);
 	}
-
 });
 
 module.exports = RacesSummaryResultsPolling;
