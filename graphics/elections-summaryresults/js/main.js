7f24075806ba7ea2f9650c4908bb3e8689640f95
Gabriel Florit <gabrielflorit@gmail.com>
2014-09-29 13:35:00 -0400

    initial elections-summaryresults commit

---
+++ graphics/elections-summaryresults/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".homepage-graphic.elections-summaryresults").length', function() {
+
+	var masterSelector = '.homepage-graphic.elections-summaryresults';
+	var master = $(masterSelector);
+
+}); }());
