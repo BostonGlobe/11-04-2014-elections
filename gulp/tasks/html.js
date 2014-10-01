6acda2ec2f3bc3975f09b4efc031467de8d11c01
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-01 14:52:22 -0400

    add csso to sass

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,21 +3,16 @@ var browserSync = require('browser-sync');
 var fileinclude = require('gulp-file-include');
 var rename      = require('gulp-rename');
 var config      = require('../config');
+var util        = require('gulp-util');
 
 gulp.task('html', function() {
 
 	var isProd = config.getUserChoice('packageToJpt');
 	var filename = isProd ? 'PROD.jpt' : 'index.html';
 
-	var task = gulp.src(config.html(isProd), {cwd: config.baseDir()})
+	return gulp.src(config.html(isProd), {cwd: config.baseDir()})
 		.pipe(fileinclude())
 		.pipe(rename(filename))
-		.pipe(gulp.dest('./', {cwd: config.baseDir()}));
-
-	if (!isProd) {
-		return task.pipe(browserSync.reload({stream:true}));
-	} else {
-		return task;
-	}
-
+		.pipe(gulp.dest('./', {cwd: config.baseDir()}))
+		.pipe(isProd ? util.noop() : browserSync.reload({stream:true}));
 });
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,12 +2,14 @@ var gulp        = require('gulp');
 var sass        = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var config      = require('../config');
+var csso        = require('gulp-csso');
+var util        = require('gulp-util');
 
 gulp.task('sass', function() {
+
 	return gulp.src('css/*', {cwd: config.baseDir()})
-		.pipe(sass({
-			compass: true
-		}))
+		.pipe(sass({compass: true}))
+		.pipe(config.getUserChoice('packageToJpt') ? csso(true) : util.noop())
 		.pipe(gulp.dest('.tmp', {cwd: config.baseDir()}))
 		.pipe(browserSync.reload({stream:true}));
 });
