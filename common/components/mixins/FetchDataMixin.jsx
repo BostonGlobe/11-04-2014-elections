7cd0058d75659569c8722e09bdbddb93297dba80
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-01 18:26:54 -0400

    refactor

--- common/components/mixins/FetchDataMixin.jsx
+++ common/components/mixins/FetchDataMixin.jsx
@@ -19,29 +19,20 @@ var FetchDataMixin = {
 	componentDidMount: function() {
 		window['_' + this.props.raceNumbers.join('_')] = function(json) {
 
-		// the races array (json) doesn't necessarily come back from the server
-		// in the order in which it was requested
-		// let's fix that right here.
+			// the races array (json) doesn't necessarily come back from the server
+			// in the order in which it was requested
+			// let's fix that right here.
 
-		var raceNumbers = this.props.raceNumbers;
+			var raceNumbers = this.props.raceNumbers;
 
-		// first: iterate over the incoming races
-		var races = _.chain(json)
-			.map(function(race) {
-
-				// get the index of first occurrence of race_number in the html attribute
-				var index = _.indexOf(raceNumbers, race.race_number);
-				return {
-					index: index,
-					race: race
-				};
-			})
-			.sortBy('index')
-			.pluck('race')
-			.value();
+			var races = _.sortBy(json, function(race) {
+				return _.indexOf(raceNumbers, race.race_number);
+			});
 
 			this.setState({races:races});
+
 		}.bind(this);
+
 		this.fetchData(true);
 	},
 	getInitialState: function() {
