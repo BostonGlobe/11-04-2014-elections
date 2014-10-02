00e24f705d9f6513a3472c7115d0af9d1bf80321
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-02 14:11:36 -0400

    enable bower requires from webpack

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -4,19 +4,26 @@ var webpackPlugins  = require('gulp-webpack/node_modules/webpack');
 var rename          = require('gulp-rename');
 var browserSync     = require('browser-sync');
 var config          = require('../config');
+var path            = require('path');
 
 gulp.task('webpack', function() {
 
 	var isProd = config.getUserChoice('packageToJpt');
 
-	var plugins = !isProd ? [] : [
-		new webpackPlugins.optimize.UglifyJsPlugin(),
-		new webpackPlugins.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify("production")
-			}
-		})
+	var plugins = [
+		new webpackPlugins.ResolverPlugin(
+			new webpackPlugins.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+		)
 	];
+
+	if (isProd) {
+		plugins.push(new webpackPlugins.optimize.UglifyJsPlugin());
+		plugins.push(new webpackPlugins.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}));
+	}
 
 	return gulp.src(config.baseDir() + '/js/entry.js')
 		.pipe(webpack({
@@ -27,7 +34,10 @@ gulp.task('webpack', function() {
 					{ test: /\.css$/, loader: 'style-loader!css-loader' }
 				]
 			},
-			plugins: plugins
+			plugins: plugins,
+			resolve: {
+				root: [path.join(__dirname, '../../libs')]
+			}
 		}))
 		.pipe(rename('bundle.js'))
 		.pipe(gulp.dest('.tmp', {cwd: config.baseDir()}))
