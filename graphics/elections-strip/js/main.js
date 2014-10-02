c4571f48d62135cc597b729cd4d7e81205bc8f92
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-02 19:01:20 -0400

    initial elections-strip commit

---
+++ graphics/elections-strip/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".homepage-graphic.elections-strip").length', function() {
+
+	var masterSelector = '.homepage-graphic.elections-strip';
+	var master = $(masterSelector);
+
+}); }());
