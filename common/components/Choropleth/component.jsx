57ef61afa0ea08d50f31908aeef1beb8572488f2
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-10 19:18:07 -0400

    compute map boundaries based on race

--- common/components/Choropleth/component.jsx
+++ common/components/Choropleth/component.jsx
@@ -111,6 +111,7 @@ var Choropleth = React.createClass({
 			});
 
 		}
+
 		var after = Date.now();
 		console.log('binding results to shapefile took ' + (after - before) + 'ms');
 
@@ -124,29 +125,6 @@ var Choropleth = React.createClass({
 			.enter().append('path')
 				.attr('class', this.chooseFillClass)
 				.attr('d', this.path);
-
-
-
-
-		// // first we'll bind results to topojson
-		// features.forEach(function(feature) {
-
-		// 	var match = _.find(results.reporting_units, function(reporting_unit) {
-		// 		return reporting_unit.county_name.toUpperCase() === feature.properties.TOWN;
-		// 	});
-
-		// 	debugger;
-
-		// });
-
-
-
-		// // get the reporting unit for this json feature town
-		// // TODO: is this the most performant way? or am i prematurely optimizing
-		// var reporting_unit = _.find(race.reporting_units, function(reporting_unit) {
-		// 	return reporting_unit.county_name.toUpperCase() === d.properties.TOWN;
-		// });
-
 	},
 
 	updateContainerDimensions: function() {
@@ -166,15 +144,26 @@ var Choropleth = React.createClass({
 		$('body').height('auto');
 	},
 
-	componentDidMount: function() {
+	createMap: function(results) {
 
 		var shapefile = this.props.shapefile;
 
 		// TODO: change name of TOWNS to something more generic.
-		var feature = topojson.feature(shapefile, shapefile.objects.TOWNS);
+		var masterRegion = topojson.feature(shapefile, shapefile.objects.TOWNS);
+
+		var townsInRace = _.chain(results.reporting_units)
+			.pluck('county_name')
+			.map(function(county_name) {
+				return county_name.toUpperCase();
+			})
+			.value();
+
+		var raceRegion = topojson.merge(shapefile, shapefile.objects.TOWNS.geometries.filter(function(d) {
+			return _.contains(townsInRace, d.properties.TOWN);
+		}));
 
 		// we'll center the map on the centroid
-		var centroid = d3.geo.centroid(feature);
+		var centroid = d3.geo.centroid(raceRegion);
 
 		// Create a unit projection.
 		var projection = d3.geo.albers()
@@ -188,7 +177,7 @@ var Choropleth = React.createClass({
 			.projection(projection);
 
 		// Compute the bounds of a feature of interest.
-		var b = this.path.bounds(feature);
+		var b = this.path.bounds(raceRegion);
 
 		// we'll use the aspect to update container dimensions
 		// we wouldn't need to do this were it not for safari
@@ -202,7 +191,7 @@ var Choropleth = React.createClass({
 		var height = Math.round(width/this.aspect);
 
 		// Compute scale and translate.
-		var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+		var s = 0.55 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
 		var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
 
 		// Update the projection to use computed scale & translate.
@@ -222,15 +211,22 @@ var Choropleth = React.createClass({
 		// create the features container
 		svg.append('g');
 
-		// create the outline
-		// TODO: is this the best way of drawing the outline?
+		// create the region outline
+		svg.append('path')
+			.datum(raceRegion)
+			.attr({
+				'd': this.path,
+				'class': 'region'
+			});
+
+		// create the master region outline
 		svg.append('path')
 			.datum(topojson.mesh(shapefile, shapefile.objects.TOWNS, function(a, b) {
 				return a === b;
 			}))
 			.attr({
 				'd': this.path,
-				'class': 'outline'
+				'class': 'master-region'
 			});
 
 		// next is the part where we draw the data
@@ -241,6 +237,12 @@ var Choropleth = React.createClass({
 	},
 
 	shouldComponentUpdate: function(props, state) {
+
+		// is this the first time we get results?
+		// if so, draw the map
+		if (!this.props.results && props.results) {
+			this.createMap(props.results);
+		}
 
 		// next is the part where we draw the data
 		this.updateMap(props.results);
