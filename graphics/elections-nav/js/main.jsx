13720464a6ee68f071acc8b539e03095f5a75b2a
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-23 18:28:23 -0400

    add campaign 2014 logo

--- graphics/elections-nav/js/main.jsx
+++ graphics/elections-nav/js/main.jsx
@@ -3,17 +3,9 @@
  */
 
 var React = require('react');
-var DetailedResults = require('../../../common/containers/DetailedResults.jsx');
-
-// governor is 22957
-// 1st Essex is 22091
-// 6th district is 22044
-// 5th Suffolk is 23098
-// something up north is 22014
-// casino is 24698
-// governor is 22796
+var ElectionNav = require('../../../common/containers/ElectionNav.jsx');
 
 React.renderComponent(
-	<DetailedResults raceNumbers={[22957]} state={'MA'} />,
-	document.getElementById('elections-detailedresults')
+	<ElectionNav />,
+	document.getElementById('elections-nav')
 );
