079277a580460b424ecb76f1b2c45100449b63b1
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-24 20:29:52 -0400

    style balance of power

--- common/components/BalanceOfPower.jsx
+++ common/components/BalanceOfPower.jsx
@@ -4,16 +4,55 @@
 
 var React = require('react');
 
+var util  = require('../assets/js/util.js');
+
 var BalanceOfPower = React.createClass({
 
 	render: function() {
-		var results = this.props.results;
+
+		var results = {};
+
+		_.chain(this.props.results.trendtable.party)
+			.forEach(function(party) {
+
+				var Won       = _.find(party.trend, {name: 'Won'}).value;
+				var Holdovers = _.find(party.trend, {name: 'Holdovers'}).value;
+				var NetChangeWinners = _.find(party.NetChange.trend, {name: 'Winners'}).value;
+
+				results[party.title] = {
+					'WonHoldovers': Won + Holdovers,
+					'NetChangeWinners': NetChangeWinners
+				};
+			});
+
+		var total =
+			results.Dem.WonHoldovers +
+			results.GOP.WonHoldovers +
+			results.Others.WonHoldovers;
+
+		var democraticBar = {
+			width: (100 * results.Dem.WonHoldovers/total) + '%'
+		};
+
+		var republicanBar = {
+			width: (100 * results.GOP.WonHoldovers/total) + '%'
+		};
 
 		return (
 			<div className='balance-of-power'>
-
-				<pre>{JSON.stringify(results, null, 4)}</pre>
-
+				<div className='bars'>
+					<div className='democratic' style={democraticBar}>&nbsp;</div>
+					<div className='republican' style={republicanBar}>&nbsp;</div>
+				</div>
+				<div className='total'>
+					<div className='democratic'><span>{results.Dem.WonHoldovers} democrats</span></div>
+					<div className='republican'><span>{results.GOP.WonHoldovers} republicans</span></div>
+					<div className=' undecided'><span>{results.Others.WonHoldovers} undecided</span></div>
+				</div>
+				<div className='change'>
+					<div className='democratic'><span>{results.Dem.NetChangeWinners} seats</span></div>
+					<div className='republican'><span>{results.GOP.NetChangeWinners} seats</span></div>
+				</div>
 			</div>
 		);
 	}
