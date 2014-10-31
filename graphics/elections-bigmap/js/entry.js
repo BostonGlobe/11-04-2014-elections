d559f572c29671da1426e09d0536218d9f6701f4
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-31 16:34:13 -0400

    setup big map plumbing

--- graphics/elections-bigmap/js/entry.js
+++ graphics/elections-bigmap/js/entry.js
@@ -1,8 +1,5 @@
 (function() { globe.onDefine('window.jQuery && $(".homepage-graphic.elections-bigmap").length', function() {
 
-	var masterSelector = '.homepage-graphic.elections-bigmap';
-	var master = $(masterSelector);
-
 	require('./main.jsx');
 
 }); }());
