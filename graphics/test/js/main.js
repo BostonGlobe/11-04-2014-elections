38469d2defc748ba235993bd596070686de0662b
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-31 13:40:35 -0400

    housekeeping

--- graphics/test/js/main.js
+++
@@ -1,6 +0,0 @@
-(function() { globe.onDefine('window.jQuery && $(".homepage-graphic.test").length', function() {
-
-	var masterSelector = '.homepage-graphic.test';
-	var master = $(masterSelector);
-
-}); }());
