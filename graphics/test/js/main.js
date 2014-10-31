99eb89b4f5de8c3ba8d7a160984fe7b749fe8c47
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-31 12:40:56 -0400

    initial test commit

---
+++ graphics/test/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".homepage-graphic.test").length', function() {
+
+	var masterSelector = '.homepage-graphic.test';
+	var master = $(masterSelector);
+
+}); }());
