d326971dc11b31d09d669be0f7d941b5c7088ccc
Daniel McLaughlin <daniel.mclaughlin@boston.com>
2014-10-31 16:11:00 -0400

    Nav done except for dropdown

--- graphics/elections-nav/js/entry.js
+++ graphics/elections-nav/js/entry.js
@@ -1,7 +1,8 @@
 (function() { globe.onDefine('window.jQuery && $("#elections-nav").length', function() {
 
-	require('./jquery-autocomplete.js');
-	require('./datalist.js');
+	// require('./jquery-autocomplete.js');
+	// require('./datalist.js');
+	require('./jquery.relevant-dropdown.js');
 	require('./main.js');
 
 }); }());
