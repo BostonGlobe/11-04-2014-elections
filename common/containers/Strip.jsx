e63a3422b73e74e15e6c68d04b40eaa6e4708ed9
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-01 16:53:15 -0400

    more refactoring

--- common/containers/Strip.jsx
+++ common/containers/Strip.jsx
@@ -2,7 +2,7 @@
  * @jsx React.DOM
  */
 
-var React = require('react');
+var React = require('react/addons');
 
 var FetchResultsMixin = require('../mixins/FetchResultsMixin.jsx');
 var PollClock         = require('../components/PollClock.jsx');
@@ -10,6 +10,73 @@ var Precincts         = require('../components/Precincts.jsx');
 var Donut             = require('../components/Donut.jsx');
 
 var util              = require('../assets/js/util.js');
+
+var Matchup = React.createClass({
+
+	render: function() {
+
+		// http://codepen.io/Kseso/pen/phiyL
+
+		// this is used to set classes
+		var cx = React.addons.classSet;
+
+		// this component only receives candidates and total votes
+		var candidates = this.props.candidates;
+		var totalVotes = this.props.totalVotes;
+
+		var matchups = _.chain(candidates)
+			.sortBy('party')
+			.map(function(candidate) {
+
+				var name = candidate.last_name;
+				var party = candidate.party;
+				var votes = candidate.vote_count;
+
+				var pct = totalVotes > 0 ?
+					util.formatPercent(votes/totalVotes, 1) :
+					0;
+
+				// this is purely for utility,
+				// a really nice way of avoiding string concatenation
+				// for class creation
+				var classes = cx({
+					'matchup-container': true,
+					'winner': candidate.winner === 'X'
+				});
+
+				return (
+					<div className={classes} key={candidate.id}>
+						<div className='fl matchup-left'> 
+							<div className='matchup-party'>{party}</div>
+							<div className='matchup-name'>{name}</div>
+							<div className='matchup-votes'>{pct}%</div>
+						</div>
+						<div className='fl matchup-middle'> 
+							<div className='matchup-pic'>
+								<img src='http://placehold.it/100x100.jpg'/>
+							</div>
+						</div>
+						<div className='fl matchup-right'> 
+							<div className='matchup-donut'>
+								<div className='donut-box'> 
+									<div className='donut-content'></div> 
+								</div>
+							</div>
+						</div>
+					</div>
+				);
+
+			})
+			.value();
+
+		return (
+			<div className='strip-section section-matchup'>
+				{matchups}
+			</div>
+		);
+	}
+
+});
 
 var Table = React.createClass({
 
@@ -101,6 +168,7 @@ var Strip = React.createClass({
 		var results = this.state.results[0];
 
 		var reporting = null;
+		var matchup = null;
 		var table = null;
 
 		if (results) {
@@ -135,116 +203,27 @@ var Strip = React.createClass({
 				return result.party === 'Dem' || result.party === 'GOP';
 			});
 
+			// get dem or gop - we could probably do this using set operations on independents,
+			// but i don't know if lodash does deep comparison
+			var mainstreamers = _.filter(extendedCandidates, function(result) {
+				return result.party === 'Dem' || result.party === 'GOP';
+			});
+
 			reporting = <Reporting name={results.office_name} reportingUnit={reportingUnit} />;
 			table = <Table totalVotes={totalVotes} candidates={independents} />;
+			matchup = <Matchup totalVotes={totalVotes} candidates={mainstreamers} />;
 		}
 
 		return (
 			<div className='strip'>
 				<PollClock ref='thePollClock' pollCallback={this.fetchResults} />
 				{reporting}
+				{matchup}
 				{table}
 			</div>
 		);
 	}
 
 });
-
-var parseData = function(results) {
-	var data = {
-		candidates: {},
-		republican: [],
-		democrat: [],
-		other: [],
-		totalVotes: 0,
-		percentReporting: null
-	};
-
-	var candidates = results['candidates'];
-	var reportingUnits = results['reporting_units'][0];
-
-	
-	candidates.forEach(function(candidate) {
-		data.candidates[candidate['id']] = { name: candidate['first_name'] + ' ' + candidate['last_name'] }
-	});
-
-	var rawPercent = reportingUnits['precincts_reporting'] / reportingUnits['total_precincts'];
-	data.percentReporting = util.formatPercent(rawPercent, 0);
-
-	reportingUnits['results'].forEach(function(candidate) {
-		var id = candidate['ap_candidate_id'];
-		var party = util.partyAbbreviationToParty(candidate['party']) || 'other';
-		var storedCandidated = data.candidates[id];
-
-		storedCandidated.party = party;
-		storedCandidated.votes = candidate['vote_count'];
-		storedCandidated.votesFormatted = util.numberWithCommas(candidate['vote_count']);
-		storedCandidated.winner = candidate['winner'] === 'X' ? 'winner' : '';
-
-		data.totalVotes += candidate['vote_count'];
-
-		data[party].push(id);
-	});
-
-	util.log(data);
-	return data;
-}
-
-var sectionMaker = {
-	matchup: function(data) {
-		
-		var d = data.candidates[data.democrat[0]];
-		var r = data.candidates[data.republican[0]];
-
-		var dClass = 'matchup-container ' + d.winner;
-		var rClass = 'matchup-container ' + r.winner;
-
-
-		var dPercent = data.totalVotes > 0 ? util.formatPercent(d.votes / data.totalVotes, 0) : 0;
-		var rPercent = data.totalVotes > 0 ? util.formatPercent(r.votes / data.totalVotes, 0) : 0;
-
-		// http://codepen.io/Kseso/pen/phiyL
-		return <div className='strip-section section-matchup'>
-					<div className={dClass}>
-						<div className='fl matchup-left'> 
-							<div className='matchup-party'>Democrat</div>
-							<div className='matchup-name'>{d.name}</div>
-							<div className='matchup-votes'>{d.votesFormatted} votes</div>
-						</div>
-						<div className='fl matchup-middle'> 
-							<div className='matchup-pic'>
-								<img src='http://placehold.it/100x100.jpg'/>
-							</div>
-						</div>
-						<div className='fl matchup-right'> 
-							<div className='matchup-donut'>
-								<div className='donut-box'> 
-									<div className='donut-content'></div> 
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className={rClass}>
-						<div className='fl matchup-left'> 
-							<div className='matchup-party'>Republican</div>
-							<div className='matchup-name'>{r.name}</div>
-							<div className='matchup-votes'>{r.votesFormatted} votes</div>
-						</div>
-						<div className='fl matchup-middle'> 
-							<div className='matchup-pic'>
-								<img src='http://placehold.it/100x100.jpg'/>
-							</div>
-						</div>
-						<div className='fl matchup-right'> 
-							<div className='matchup-donut'>
-								<div className='donut-box'> 
-									<div className='donut-content'></div> 
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>;
-	}
-};
 
 module.exports = Strip;
