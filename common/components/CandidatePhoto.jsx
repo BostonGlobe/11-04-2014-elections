4e83922dbcddbb93188c0b602bcedfb28998087b
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-03 16:42:15 -0500

    make photos programmatical

--- common/components/CandidatePhoto.jsx
+++ common/components/CandidatePhoto.jsx
@@ -13,15 +13,17 @@ var CandidatePhoto = React.createClass({
 
 		filename: function(opts) {
 
-			var office = opts.alternate_office_name;
-			var seat = opts.seat_name;
+			var office = opts.alternate_office_name.replace(/\W/g, '');
+			var seat = opts.seat_name.replace(/\W/g, '');
 			var state = opts.state_postal;
 			var moment = Moment(opts.election_date);
 			var displayDate = moment.format('YYYY-MM-DD');
 
-			var entry = _.filter([displayDate, state, office, seat], function(v) {
+			var entry = _.chain([displayDate, state, office, seat])
+				.filter(function(v) {
 					return v;
-				}).join('-');
+				})
+				.value().join('-').toLowerCase();
 
 			return entry;
 		},
@@ -29,7 +31,9 @@ var CandidatePhoto = React.createClass({
 		exists: function(opts) {
 
 			var list = [
-				'2014-11-04-MA-Governor'
+				'2014-11-04-ma-governor',
+				'2014-11-04-ma-attorneygeneral',
+				'2014-11-04-ma-auditor'
 			];
 
 			var filename = CandidatePhoto.filename(opts);
@@ -41,12 +45,13 @@ var CandidatePhoto = React.createClass({
 
 	render: function() {
 
-		var filename = [CandidatePhoto.filename(this.props.race), this.props.candidate.last_name].join('-');
+		var filename = [CandidatePhoto.filename(this.props.race), this.props.candidate.last_name.replace(/\W/g, '')].join('-').toLowerCase();
+		var url = ['http://private.boston.com/multimedia/graphics/projectFiles/2014/11/assets/candidate-photos/', filename, '.png'].join('');
 
 		return (
 			<td className='candidate-photo' rowSpan='2'>
 				<div>
-					<img src='http://c.o0bg.com/rf/image_80x80/Boston/Library/Staff/Caricatures/abrahamy.png' />
+					<img src={url} />
 				</div>
 			</td>
 		);
