b83becfc374d5779dfa8de3db2add2393cef20c9
Daniel McLaughlin <daniel.mclaughlin@boston.com>
2014-11-03 13:30:39 -0500

    added linkbar

---
+++ graphics/elections-linkbar/js/main.js
@@ -0,0 +1,25 @@
+var $linkbar = $('#elections-linkbar');
+
+new ResizeSensor($linkbar, setBreakpointClasses);
+setBreakpointClasses();
+
+function setBreakpointClasses() {
+	var w = $linkbar.width();
+	$linkbar.toggleClass('linkbar-breakpoint-hide-links',w<900)
+		.toggleClass('linkbar-breakpoint-show-toggle',w<600);
+}
+
+$('.elections-linkbar__toggle').click(function() {
+	var panel = $(this).data('panel');
+	var alreadyActive = $(this).hasClass('elections-linkbar__toggle--active');
+	$('.elections-linkbar__toggle--active').removeClass('elections-linkbar__toggle--active');
+	$('.elections-linkbar__panel--active').removeClass('elections-linkbar__panel--active');
+	if (!alreadyActive) {
+		$('.elections-linkbar__toggle-'+panel).addClass('elections-linkbar__toggle--active');
+		$('.elections-linkbar__panel-'+panel).addClass('elections-linkbar__panel--active')
+			.find('input,select')
+				.focus()
+				.val("");
+	}
+});
+
