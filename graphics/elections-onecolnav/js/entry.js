1157c928a8a682dafb86cc45265d0de1d418016e
Daniel McLaughlin <daniel.mclaughlin@boston.com>
2014-11-03 17:07:05 -0500

    Added onecol nav, which I guess we don't need. You were too pure for this world, little nav.

---
+++ graphics/elections-onecolnav/js/entry.js
@@ -0,0 +1,7 @@
+(function() { globe.onDefine('window.jQuery && $("#elections-onecolnav").length', function() {
+
+    // require('./ResizeSensor.js');
+	require('./jquery.relevant-dropdown.js');
+	require('./main.js');
+
+}); }());
