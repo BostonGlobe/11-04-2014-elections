7e74a8b55bce8618a6063d03dbcaf1b040468ce2
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-03 20:04:56 -0500

    make bop work

--- common/components/BalanceOfPower.jsx
+++ common/components/BalanceOfPower.jsx
@@ -4,7 +4,7 @@
 
 var React    = require('react');
 
-var RaceName = require('./RaceName.jsx');
+var Title    = require('./Title.jsx');
 
 var util     = require('../assets/js/util.js');
 
@@ -12,69 +12,73 @@ var BalanceOfPower = React.createClass({
 
 	statics: {
 
-		convertFeedToResults: function(feed) {
+		transformResults: function(input) {
 
-			var results = {};
+			var results = {
+				name: input.trendtable.office.replace(/\./g, '')
+			};
 
-			_.chain(feed.trendtable.party)
-				.forEach(function(party) {
+			var parties = input.trendtable.party.map(function(party) {
 
-					var Won       = _.find(party.trend, {name: 'Won'}).value;
-					var Holdovers = _.find(party.trend, {name: 'Holdovers'}).value;
-					var NetChangeWinners = _.find(party.NetChange.trend, {name: 'Winners'}).value;
+				var Won       = _.find(party.trend, {name: 'Won'}).value;
+				var Holdovers = _.find(party.trend, {name: 'Holdovers'}).value;
+				var Leading   = _.find(party.trend, {name: 'Leading'}).value;
+				var NetChangeWinners = _.find(party.NetChange.trend, {name: 'Winners'}).value;
 
-					results[party.title] = {
-						'WonHoldovers': Won + Holdovers,
-						'NetChangeWinners': NetChangeWinners
-					};
-				});
+				return {
+					name: party.title,
+					'WonHoldovers': Won + Holdovers,
+					'NetChangeWinners': NetChangeWinners,
+					'Leading': Leading
+				};
+			});
+
+			results.parties = parties;
 
 			return results;
-		},
-
-		getNameFromFeed: function(feed) {
-
-			return feed.trendtable.office.replace('U.S.', 'US');
-
 		}
 
 	},
 
 	render: function() {
 
-		var results = BalanceOfPower.convertFeedToResults(this.props.results);
-
-		var total =
-			results.Dem.WonHoldovers +
-			results.GOP.WonHoldovers +
-			results.Others.WonHoldovers;
-
-		var democratBar = {
-			width: (100 * results.Dem.WonHoldovers/total) + '%'
-		};
-
-		var republicanBar = {
-			width: (100 * results.GOP.WonHoldovers/total) + '%'
-		};
-
 		return (
-			<div className='balance-of-power'>
-				<RaceName name={BalanceOfPower.getNameFromFeed(this.props.results)} />
-				<div className='bars'>
-					<div className='democrat' style={democratBar}>&nbsp;</div>
-					<div className='republican' style={republicanBar}>&nbsp;</div>
-				</div>
-				<div className='total'>
-					<div className='democrat'><span>{results.Dem.WonHoldovers} democrats</span></div>
-					<div className='republican'><span>{results.GOP.WonHoldovers} republicans</span></div>
-					<div className=' undecided'><span>{results.Others.WonHoldovers} undecided</span></div>
-				</div>
-				<div className='change'>
-					<div className='democrat'><span>{results.Dem.NetChangeWinners} seats</span></div>
-					<div className='republican'><span>{results.GOP.NetChangeWinners} seats</span></div>
-				</div>
-			</div>
+			<pre>{JSON.stringify(this.props, null, 4)}</pre>
 		);
+
+		// var results = this.props.results;
+
+		// var total =
+		// 	results.Dem.WonHoldovers +
+		// 	results.GOP.WonHoldovers +
+		// 	results.Others.WonHoldovers;
+
+		// var democratBar = {
+		// 	width: (100 * results.Dem.WonHoldovers/total) + '%'
+		// };
+
+		// var republicanBar = {
+		// 	width: (100 * results.GOP.WonHoldovers/total) + '%'
+		// };
+
+		// return (
+		// 	<div className='balance-of-power'>
+		// 		<RaceName name={BalanceOfPower.getNameFromFeed(this.props.results)} />
+		// 		<div className='bars'>
+		// 			<div className='democrat' style={democratBar}>&nbsp;</div>
+		// 			<div className='republican' style={republicanBar}>&nbsp;</div>
+		// 		</div>
+		// 		<div className='total'>
+		// 			<div className='democrat'><span>{results.Dem.WonHoldovers} democrats</span></div>
+		// 			<div className='republican'><span>{results.GOP.WonHoldovers} republicans</span></div>
+		// 			<div className=' undecided'><span>{results.Others.WonHoldovers} undecided</span></div>
+		// 		</div>
+		// 		<div className='change'>
+		// 			<div className='democrat'><span>{results.Dem.NetChangeWinners} seats</span></div>
+		// 			<div className='republican'><span>{results.GOP.NetChangeWinners} seats</span></div>
+		// 		</div>
+		// 	</div>
+		// );
 	}
 
 });
